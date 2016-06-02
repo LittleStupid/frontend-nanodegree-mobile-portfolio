f9c72026027d773a4a219f6d2f61acfb2973ac52
LittleStupid <lttlstpd@live.com>
2016-06-02 17:32:21 +0800

    optimize

--- README.md
+++ README.md
@@ -2,5 +2,12 @@
   1.move 'load css' code into foot tags
   2.delete <script src="http://www.google-analytics.com/analytics.js"></script>
 
-# How to make pizza page scroll smoothly:
-  refactor functon updatePositions() in main.js, set document.body.scrollTop into a temp variable to avoid access the DOM every loop.
+# How to optimize pizza page:
+1.use getElementById/getElementsByClassName instead of querySelector.
+2.don't set styles every time, batch them.
+3.don't call function every time if one call is enough to provide data.
+
+### About bugs
+I found the bug "pizza size is not the same" still happened in the original version.
+In pizza.html, only col-md-xx is provided, I think some responsive bug will happen because
+of this.
--- views/js/main.js
+++ views/js/main.js
@@ -424,7 +424,7 @@ var resizePizzas = function(size) {
    // Returns the size difference to change a pizza element from one size to another. Called by changePizzaSlices(size).
   function determineDx (elem, size) {
     var oldWidth = elem.offsetWidth;
-    var windowWidth = document.querySelector("#randomPizzas").offsetWidth;
+    var windowWidth = document.getElementById("randomPizzas").offsetWidth;
     var oldSize = oldWidth / windowWidth;
 
     // Changes the slider value to a percent width
@@ -448,14 +448,27 @@ var resizePizzas = function(size) {
   }
 
   // Iterates through pizza elements on the page and changes their widths
+  //optimize : change style once, not every time
+  //call determineDx once
   function changePizzaSizes(size) {
-    var pizzaContainer = document.querySelectorAll(".randomPizzaContainer");
+    var pizzaContainer = document.getElementsByClassName("randomPizzaContainer");
     var length = pizzaContainer.length;
+    var widthContainer = [];
+
+    if( length <= 0 ) {
+      return ;
+    }
+
+    var dx = determineDx(pizzaContainer[0], size);
 
     for (var i = 0; i < length; i++) {
-      var dx = determineDx(pizzaContainer[i], size);
-      var newwidth = (pizzaContainer[i].offsetWidth + dx) + 'px';
-      pizzaContainer[i].style.width = newwidth;
+      widthContainer.push(pizzaContainer[i].offsetWidth + dx);
+      //var newwidth = (pizzaContainer[i].offsetWidth + dx) + 'px';
+      //pizzaContainer[i].style.width = newwidth;
+    }
+
+    for( var j = 0; j < length; j++ ) {
+      pizzaContainer[j].style.width = widthContainer[j] + 'px';
     }
   }
 
@@ -500,13 +513,15 @@ function logAverageFrame(times) {   // times is the array of User Timing measure
 // https://www.igvita.com/slides/2012/devtools-tips-and-tricks/jank-demo.html
 
 // Moves the sliding background pizzas based on scroll position
+//optimize: don't access document.body.scrollTop every time.
 function updatePositions() {
   frame++;
   window.performance.mark("mark_start_frame");
 
-  var items = document.querySelectorAll('.mover');
+  var items = document.getElementsByClassName('mover');
+  var top = document.body.scrollTop / 1250;
   for (var i = 0; i < items.length; i++) {
-    var phase = Math.sin((document.body.scrollTop / 1250) + (i % 5));
+    var phase = Math.sin(top + (i % 5));
     items[i].style.left = items[i].basicLeft + 100 * phase + 'px';
   }
 
@@ -535,7 +550,7 @@ document.addEventListener('DOMContentLoaded', function() {
     elem.style.width = "73.333px";
     elem.basicLeft = (i % cols) * s;
     elem.style.top = (Math.floor(i / cols) * s) + 'px';
-    document.querySelector("#movingPizzas1").appendChild(elem);
+    document.getElementById("movingPizzas1").appendChild(elem);
   }
   updatePositions();
 });
